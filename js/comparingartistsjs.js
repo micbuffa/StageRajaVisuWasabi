d032b54b60abdf5963d103a9785b62bf0e1ed9ea
oceanrj <oceanrj@users.noreply.github.com>
2020-04-18 00:38:55 +0100

    comparing page 2

--- js/comparingartistsjs.js
+++ js/comparingartistsjs.js
@@ -44,6 +44,7 @@ second={
         "begin": "1981-10",
         "end": ""
     },
+    "deezerFans":1039424,
     "location": {
         "country": "United States",
         "city": "Los Angeles"
@@ -105,6 +106,68 @@ for(i=0; i<data.length;i++){
 txt += '</tbody>'+
 '</table>';
 
-document.getElementById("demo").innerHTML = txt;
+document.getElementById("demo").innerHTML = txt;}
+
+
+function comparing(){
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 30, bottom: 40, left: 100},
+    width = 360 - margin.left - margin.right,
+    height = 300 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#my_dataviz")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+  // Add X axis
+  var x = d3.scaleLinear()
+    .domain([0, 5000000])
+    .range([ 0, width]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+      .attr("transform", "translate(-10,0)rotate(-45)")
+      .style("text-anchor", "end");
+
+// Y axis
+var y = d3.scaleBand()
+  .range([ 0, height ])
+  .domain(data.map(function(d) { return d.name; }))
+  .padding(1);
+svg.append("g")
+  .call(d3.axisLeft(y))
+
+
+// Lines
+svg.selectAll("myline")
+  .data(data)
+  .enter()
+  .append("line")
+    .attr("x1", function(d) { return x(d.deezerFans); })
+    .attr("x2", x(0))
+    .attr("y1", function(d) { return y(d.name); })
+    .attr("y2", function(d) { return y(d.name); })
+    .attr("stroke", "grey")
+
+// Circles
+svg.selectAll("mycircle")
+  .data(data)
+  .enter()
+  .append("circle")
+    .attr("cx", function(d) { return x(d.deezerFans); })
+    .attr("cy", function(d) { return y(d.name); })
+    .attr("r", "4")
+    .style("fill", "#69b3a2")
+    .attr("stroke", "black")
+
+
+
+
 }
 
